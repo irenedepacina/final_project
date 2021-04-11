a946194bb8490957ff100a3cfffb7d56c6d43ef3
irenedepacina <i.depacina@gmail.com>
2021-04-10 23:54:12 -0400

    updated html, css, js files

--- Dashboard/static/js/main.js
+++ Dashboard/static/js/main.js
@@ -1,13 +1,19 @@
-// Option 2 - jQuery Smooth Scrolling
-$('.navbar a').on('click', function (e) {
-  if (this.hash !== '') {
-    e.preventDefault();
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-    const hash = this.hash;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-    $('html, body')
-      .animate({
-        scrollTop: $(hash).offset().top
-      },800);
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-});
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
